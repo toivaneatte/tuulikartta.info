c30d91ecf27958bff2ea6b07f854538aad0c7ef9
Anni Laurila <anni.laurila@outlook.com>
2026-03-18 15:45:13 +0200

    Skip over old stations

--- php/dataMiner.php
+++ php/dataMiner.php
@@ -253,7 +253,6 @@ class DataMiner{
     public function getRoadData($timestamp, $roadSettings, $debug = false) {
         $WEATHER_API_BASE = "https://tie.digitraffic.fi/api/weather/v1";
         $user = 'Tuulen Viemät';
-
         $options = [
             'http' => [
                 'method' => "GET",
@@ -263,17 +262,14 @@ class DataMiner{
             ]
         ];
         $context = stream_context_create($options);
-
         // Load metadata
         $metadataUrl = $WEATHER_API_BASE . "/stations";
         $metadataJson = gzdecode(file_get_contents($metadataUrl, false, $context));
         $stations = json_decode($metadataJson, true);
-
         // Load sensor data
         $dataUrl = $metadataUrl . "/data";
         $stationJson = gzdecode(file_get_contents($dataUrl, false, $context));
         $allSensorData = json_decode($stationJson, true);
-
         // Index sensors by station ID
         $indexedSensors = [];
         if (isset($allSensorData['stations'])) {
@@ -281,7 +277,6 @@ class DataMiner{
                 $indexedSensors[$sData['id']] = $sData['sensorValues'];
             }
         }
-
         // Map Digitraffic → Tuulikartta names
         $roadParamMap = [
             "SADE_INTENSITEETTI" => "ri_10min",
@@ -292,16 +287,15 @@ class DataMiner{
             "LUMEN_MÄÄRÄ1" => "snow_aws",
             "KASTEPISTE" => "dewpoint"
         ];
-
         $result = [];
-
         foreach ($stations['features'] as $station) {
             $stationId = $station['properties']['id'];
-
+            if ($station['properties']['collectionStatus'] !== "GATHERING") {
+                continue;
+            }
             if (!isset($indexedSensors[$stationId])) {
                 continue;
             }
-
             // Create a single entry per station
             $entry = [
                 "station"   => $station['properties']['name'],
@@ -312,28 +306,32 @@ class DataMiner{
                 "time"      => null,
                 "epochtime" => null
             ];
-
+            $hasRecentSensor = false;
             // Add all mapped sensor values
             foreach ($indexedSensors[$stationId] as $sensor) {
+                if (!isset($sensor['measuredTime'])) {
+                    continue;
+                }
+                $sensorTime = strtotime($sensor['measuredTime']);
+                if (time() - $sensorTime > 60 * 60 * 24) { // Only consider sensors updated within the last day
+                    continue;
+                }
+                $hasRecentSensor = true;
                 $rawName = $sensor['name'];
-
                 if (isset($roadParamMap[$rawName])) {
                     $mappedName = $roadParamMap[$rawName];
                     $entry[$mappedName] = floatval($sensor['value']);
-
                     // Use the newest timestamp
                     $entry["time"] = $sensor['measuredTime'];
                     $entry["epochtime"] = strtotime($sensor['measuredTime']);
                 }
             }
-
-            $result[] = $entry;
+            if ($hasRecentSensor) {
+                $result[] = $entry;
+            }
         }
-
         return $result;
     }
-
-
 
 
     /** Parse observation data about R-values from RWC
